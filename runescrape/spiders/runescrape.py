e6f14fae9c4a8bd51417b4c5740f192500ccbb87
Luke Van Seters <lukevanseters@gmail.com>
2021-08-29 12:50:39 -0400

    Add parsing items from child pages

--- runescrape/spiders/runescrape.py
+++ runescrape/spiders/runescrape.py
@@ -1,10 +1,26 @@
 import scrapy
+from pydantic import BaseModel
+
+
+class RunscapeItem(BaseModel):
+    title: str
+    icon_source_url: str
 
 
 class RunscrapeSpider(scrapy.Spider):
     name = 'runescrape'
-    allowed_domains = ['oldschool.runescape.wiki/w/Head_slot_table']
+    allowed_domains = ['oldschool.runescape.wiki']
     start_urls = ['https://oldschool.runescape.wiki/w/Head_slot_table']
 
     def parse(self, response):
-        pass
+        article_links = response.css(".wikitable tr td a")
+        yield from response.follow_all(article_links, callback=self.parse_article)
+
+    def parse_article(self, response):
+        title = response.css("#firstHeading::text").get()
+        icon_source_url = response.css(".infobox-image img")[-1].attrib["src"]
+
+        yield RunscapeItem(
+            title=title,
+            icon_source_url=icon_source_url,
+        )
